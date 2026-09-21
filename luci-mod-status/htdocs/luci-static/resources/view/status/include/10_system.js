20b99ba9b3cb382e5290da9544656b719d1c432d
hizhangdh <131931857+hizhangdh@users.noreply.github.com>
2026-09-21 11:06:48 +0800

    Update 10_system.js

--- luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+++ luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
@@ -88,11 +88,6 @@ return baseclass.extend({
 			_('Architecture'),     (cpuinfo.cpuinfo || boardinfo.system) + ' ' + cpubench.cpubench,
 			_('Target Platform'),  (L.isObject(boardinfo.release) ? boardinfo.release.target : ''),
 			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
-				? '%s%s / '.format(
-					boardinfo.release.description || '',
-					boardinfo.release.revision ? boardinfo.release.revision : ''
-				)
-				: '') + (luciversion || ''),
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
 			_('Uptime'),           systeminfo.uptime ? '%t'.format(systeminfo.uptime) : null,
